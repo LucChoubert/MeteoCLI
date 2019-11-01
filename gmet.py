c0eba0aa8add6cd0fdaa32841ce12776eb2e2372
Luc Choubert <luc.choubert@gmail.com>
2019-11-01 16:35:58 +0100

    working version with -s and -o option

--- gmet.py
+++ gmet.py
@@ -13,6 +13,8 @@ from urllib.request import urlopen
 def parse() :
     parser = argparse.ArgumentParser(description='Command Line utility to access Meteo data from various sources')
     parser.add_argument('--city', '-c', dest='city', help='cityname, if no value is provided, automatically guessed via the geolocalized IP adress')
+    parser.add_argument('--offset', '-o', dest='offset', type=int, default=0, help='offset in days compared to today, i.e. 0 means today, 1 means tomorrow,...')
+    parser.add_argument('--summary', '-s', dest='summary', action='store_true', help='summary view, i.e. gives all data received from server in a synthetic way')
     return parser.parse_args()
 
 
@@ -22,14 +24,7 @@ def localize() :
     response = urlopen(url)
     data = json.load(response)
 
-    IP=data['ip']
-    city = data['city']
-    postal = data['postal']
-    region = data['region']
-    country = data['country']
-    org = data['org']
-
-    d = dict();
+    d = dict()
     d['ip'] = data['ip']
     d['city'] = data['city']
     d['zip']   = data['postal']
@@ -54,12 +49,26 @@ def getDataFromMeteoFranceAPI( iCityInseeCode ):
     url = 'http://ws.meteofrance.com/ws/getDetail/france/' + iCityInseeCode + '.json'
     response = urlopen(url)
     data = json.load(response)
-    #timeString = time.strftime("%d %b %Y, %a", time.gmtime(data['result']['resumes']['0_resume']['date'] / 1000))
-    #print(timeString + " T-Min:{1} T-Max:{2} Météo: {0}".format(data['result']['resumes']['0_resume']['description'],data['result']['resumes']['0_resume']['temperatureMin'],data['result']['resumes']['0_resume']['temperatureMax']))
-    for i in range(0, 9):
-        keyString = str(i)+'_resume'
-        timeString = time.strftime("%d %b %Y, %a", time.gmtime(data['result']['resumes'][keyString]['date'] / 1000))
-        print(timeString + " T-Min:{1} T-Max:{2} Météo: {0}".format(data['result']['resumes'][keyString]['description'],data['result']['resumes'][keyString]['temperatureMin'],data['result']['resumes'][keyString]['temperatureMax']))    
+    return data
+
+def formatOutput(iConfig, iData):
+    #timeString = time.strftime("%d %b %Y, %a", time.gmtime(iData['result']['resumes']['0_resume']['date'] / 1000))
+    #print(timeString + " T-Min:{1} T-Max:{2} Météo: {0}".format(iData['result']['resumes']['0_resume']['description'],iData['result']['resumes']['0_resume']['temperatureMin'],iData['result']['resumes']['0_resume']['temperatureMax']))
+    myRange = range(iConfig.offset, iConfig.offset+1)
+    if iConfig.summary:
+        myRange = range(0, 100)
+    for i in myRange:
+        #Display the 10 days of daily prevision in resumes section
+        keyResumes = str(i)+'_resume'
+        if keyResumes in iData['result']['resumes']:
+            timeString = time.strftime("%a-%d%b", time.gmtime(iData['result']['resumes'][keyResumes]['date'] / 1000))
+            print("{} T-Min:{} T-Max:{} Météo: {}".format(timeString, iData['result']['resumes'][keyResumes]['temperatureMin'], iData['result']['resumes'][keyResumes]['temperatureMax'], iData['result']['resumes'][keyResumes]['description']))
+
+        #Then, display the prevision "by range matin, midi, soir, nuit" in previsions section
+        for r in ['matin', 'midi', 'soir', 'nuit']:
+            keyPrevisions = str(i)+'_'+r
+            if keyPrevisions in iData['result']['previsions']:
+                print('{:>9} T-Min:{} T-Max:{} Météo: {}'.format(r, iData['result']['previsions'][keyPrevisions]['temperatureMin'], iData['result']['previsions'][keyPrevisions]['temperatureMax'], iData['result']['previsions'][keyPrevisions]['description']))
 
 #Main function
 if __name__ == '__main__':
@@ -72,5 +81,6 @@ if __name__ == '__main__':
         data['city'] = args.city
     data['insee'], data['zip'], data['country'] = getInseeCode(data['city'])
 
-    print('Meteo forecast -- City: {1}  Zip: {2}  Insee: {3}  Country: {4}'.format(data['ip'],data['city'],data['zip'],data['insee'],data['country']))    
-    getDataFromMeteoFranceAPI(data['insee'])
+    print('Meteo forecast -- City: {0}  Zip: {1}  Insee: {2}  Country: {3}'.format(data['city'],data['zip'],data['insee'],data['country']))    
+    data = getDataFromMeteoFranceAPI(data['insee'])
+    formatOutput(args, data)
