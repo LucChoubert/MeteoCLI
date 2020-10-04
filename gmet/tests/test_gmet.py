bca6c2f3eb82ecc1655035292cf38bb59f86b1ef
Luc Choubert <luc.choubert@gmail.com>
2020-10-04 22:43:45 +0200

    Firust unit test implemented

--- gmet/tests/test_gmet.py
+++ gmet/tests/test_gmet.py
@@ -1,4 +1,16 @@
+import pytest
 from gmet import gmet
 
-def test_localize():
-    assert gmet.localize() == 4
+def test_getInseeCode():
+        assert gmet.getInseeCode("paris")[0] == "750560"
+        assert gmet.getInseeCode("paris", "751070")[0] == "751070"
+        assert gmet.getInseeCode("bordeaux")[0] == "330630"
+        assert gmet.getInseeCode("bordeaux", "450410")[0] == "450410"
+
+def test_getInseeCode_exception1():
+    with pytest.raises(ValueError):
+        gmet.getInseeCode("nomatch")
+
+def test_getInseeCode_exception2():
+    with pytest.raises(ValueError):
+        gmet.getInseeCode("bordeaux", "993366")
